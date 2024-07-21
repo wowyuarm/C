57306392c905260e4e36326309e3e6d2489049ac
wowyuarm <wuyuang05@outlook.com>
2024-07-21 13:07:26 +0800

    Update testC2.c

--- testC2.c
+++ testC2.c
@@ -1,31 +1,12 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.10.35027.167
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "testC2", "testC2\testC2.vcxproj", "{E04D2AB1-09A6-423A-A3CD-937490C05D63}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Debug|x64.ActiveCfg = Debug|x64
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Debug|x64.Build.0 = Debug|x64
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Debug|x86.ActiveCfg = Debug|Win32
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Debug|x86.Build.0 = Debug|Win32
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Release|x64.ActiveCfg = Release|x64
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Release|x64.Build.0 = Release|x64
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Release|x86.ActiveCfg = Release|Win32
-		{E04D2AB1-09A6-423A-A3CD-937490C05D63}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {9AD584B3-49CE-444D-A20B-911DAB52EEDC}
-	EndGlobalSection
-EndGlobal
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
