dc88e9fcb12057c2d1d8d7bb17cbe730a8cc6c75
wowyuarm <wuyuang05@outlook.com>
2024-07-23 17:29:45 +0800

    C5

--- testC5.c
+++ testC5.c
@@ -5,7 +5,7 @@ int main()
 	int num2 = 0;
 	int sum = 0;
 	printf("请输入两个数字:>");
-	scanf("%d %d", &num1,&num2);
+	scanf("%d %d", &num1, &num2);
 	sum = num1 + num2;
 	printf("sum=%d\n", sum);
 	return 0;
