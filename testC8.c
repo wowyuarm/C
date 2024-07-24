dbbef381427c6517d5a3e35acd66ccaab057f896
wowyuarm <wuyuang05@outlook.com>
2024-07-24 16:27:52 +0800

    C8

---
+++ testC8.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+int main()
+{
+
+	{
+		printf("%c\n", '\'');
+	}
+	{
+		printf("%c\n", "\"");
+	}
+	{
+		printf("\a\n");
+	}
+	{
+		printf("c:\\testC8\\testC8.c\n");
+	}
+
+	{
+		printf("%c", '\110');
+		printf("%c", '\105');
+		printf("%c", '\114');
+		printf("%c", '\114');
+		printf("%c\n", '\117');  //输出HELLO
+	}
+
+	return 0;
+}
