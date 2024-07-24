370eafd6966c536b2d395396d57954e06703ad4b
wowyuarm <wuyuang05@outlook.com>
2024-07-24 15:04:13 +0800

    C7

---
+++ testC7.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c','\0' };
+
+	printf("%d", strlen(arr1));
+	printf("%d", strlen(arr2));
+
+	return 0;
+}
