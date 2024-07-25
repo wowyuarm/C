d6a1bc57585c554a318eec35bd3206fa4cde798b
wowyuarm <wuyuang05@outlook.com>
2024-07-25 16:54:12 +0800

    C9

---
+++ testC9.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int num = 0;
+	printf("1 for rich or 0 for poor\n");
+	printf("please make your choice\n");
+	scanf("%d", &num);
+
+	if (num == 1)
+		printf("You are the millionaire");
+	else
+		printf("You are the millionaire of the future");
+
+	return 0;
+}
