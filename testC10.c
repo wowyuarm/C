2ecd4d8bc5425df20684a50b549bdd6883be2132
wowyuarm <wuyuang05@outlook.com>
2024-07-25 17:12:54 +0800

    C10

---
+++ testC10.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main()
+{
+	int num = 0;
+	while (num < 58094194)
+	{
+		printf("wait:%d", num);
+		num = num + 1;
+	}
+	if (num == 58094194)
+	{
+		printf("This is my QQ num");
+	}
+	return 0;
+}
