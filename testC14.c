5615d8be88f4666af5c304d67f15f99242f66241
wowyuarm <wuyuang05@outlook.com>
2024-07-29 18:00:02 +0800

    C14

---
+++ testC14.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday");
+		break;
+	case 6:
+	case 7:
+		printf("weekend");
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
