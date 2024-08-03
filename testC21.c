d593247b5f195046762007400b225902285978e6
wowyuarm <wuyuang05@outlook.com>
2024-08-03 18:31:03 +0800

    练习-C21

---
+++ testC21.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+void happy(void);
+int main()
+{
+	happy(), happy(), happy();
+	printf("\n");
+	happy(), happy();
+	printf("\n");
+	happy();
+
+	return 0;
+}
+void happy(void)
+{
+	printf("Smile!");
+}
