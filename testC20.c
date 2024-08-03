cb0ea0e5bb914a3e8657c94756a185eb97f6c21e
wowyuarm <wuyuang05@outlook.com>
2024-08-03 18:30:41 +0800

    练习-C20

---
+++ testC20.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main()
+{
+	int toes, double_toes, square_toes;
+
+	toes = 10;
+	double_toes = 2 * toes;
+	square_toes = toes * toes;
+	printf("%d\n%d\n%d", toes, double_toes, square_toes);
+
+	return 0;
+}
