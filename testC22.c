1e8519b8bb3d7b8785b54b7c06f4f7174b391eed
wowyuarm <wuyuang05@outlook.com>
2024-08-03 18:33:17 +0800

    练习-C22

---
+++ testC22.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+void one_three(void);
+void two(void);
+int main()
+{
+	printf("starting now:\n");
+	one_three();
+
+	return 0;
+}
+void one_three(void)
+{
+	printf("one\n");
+	two();
+	printf("three\ndone!");
+}
+void two(void)
+{
+	printf("two\n");
+}
