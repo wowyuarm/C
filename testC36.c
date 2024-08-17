4f23836187a6b08b1e17b79391c4f0257bdfeb2c
wowyuarm <wuyuang05@outlook.com>
2024-08-17 17:58:57 +0800

    C36

---
+++ testC36.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main(void)
+{
+	float salary;
+
+	printf("\aEnter your designed monthly salary:");
+	printf(" $________\b\b\b\b\b\b\b\b");
+	scanf("%f", &salary);
+	printf("\n\t$%.3f a month is $%.3f a year", salary, salary * 12);
+	printf("\rGee!\n");
+
+	return 0;
+}
