75f06f1746721a1120aa0a4419e77afbe41d8d82
wowyuarm <wuyuang05@outlook.com>
2024-08-19 17:18:17 +0800

    练习-C39

---
+++ testC39.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	float num = 0;
+
+	printf("Enter a floating-point value:");
+	scanf("%f", &num);
+	printf("fixed-point notation:%.6f\n", num);
+	printf("exponential notation:%e\n", num);
+	printf("p notation:%a\n", num);
+
+	return 0;
+}
