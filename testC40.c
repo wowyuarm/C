a3c16cbeefd857966d234ecb7969ec439cb56128
wowyuarm <wuyuang05@outlook.com>
2024-08-20 17:41:26 +0800

    练习-C40

---
+++ testC40.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	float molecule = 3.0e-23;
+	float quart = 950;
+	float quarts, molecules;
+
+	printf("Enter the number of quart of water:");
+	scanf("%f", &quarts);
+	molecules = quarts * quart / molecule;
+	printf("%f quarts of water contain %e molecules.\n", quarts, molecules);
+
+	return 0;
+}
