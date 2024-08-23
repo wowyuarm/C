759999c3c980ebeafe750c6c3c9fae9d1ee0cab1
wowyuarm <wuyuang05@outlook.com>
2024-08-23 18:09:02 +0800

    C43

---
+++ testC43.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#define C "123456789"
+int main(void)
+{
+	int A = 123456789;
+	float B = 0.123456;
+
+	printf("*%010d*\n", A);
+	printf("**%8.6f**\n", B);
+	printf("***%-10s***", C);
+	return 0;
+}
