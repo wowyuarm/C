cea35a6d94bc78d936b79f97066b85f83b58202d
wowyuarm <wuyuang05@outlook.com>
2024-08-24 17:31:53 +0800

    C44

---
+++ testC44.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main(void)
+{
+	unsigned width;
+	int num;
+
+	printf("Enter three number:");
+	scanf("%*d %*d %d", &num);
+	printf("Enter the width:");
+	scanf("%d", &width);
+	printf("The last number is:%*d", width, num);
+
+	return 0;
+}
