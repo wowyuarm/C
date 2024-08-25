6aca205ed2a0a0cdda5b48d052427b3982535081
wowyuarm <wuyuang05@outlook.com>
2024-08-25 17:45:49 +0800

    练习-C45

---
+++ testC45.c
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.10.35027.167
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "testC45", "testC45\testC45.vcxproj", "{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Debug|x64.ActiveCfg = Debug|x64
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Debug|x64.Build.0 = Debug|x64
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Debug|x86.ActiveCfg = Debug|Win32
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Debug|x86.Build.0 = Debug|Win32
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Release|x64.ActiveCfg = Release|x64
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Release|x64.Build.0 = Release|x64
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Release|x86.ActiveCfg = Release|Win32
+		{11E7B89F-17D0-40D2-8319-59D3AA3DE97C}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {1C725498-0EF4-4AD1-8357-4724C6509E11}
+	EndGlobalSection
+EndGlobal
