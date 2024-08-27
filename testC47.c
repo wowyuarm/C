04f441ce02fae2898aa891c8a2a107d5f40c7e3e
wowyuarm <wuyuang05@outlook.com>
2024-08-27 17:54:42 +0800

    C47

---
+++ testC47.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#define SEC_PER_MIN 60
+int main()
+{
+	int sec = 0;
+	printf("Enter the number of second:");
+	scanf("%d", &sec);
+	if (sec <= SEC_PER_MIN)
+	{
+		printf("It's %dsecond!", sec);
+	}
+	else
+	{
+		printf("%dminute %dsecond!", sec / SEC_PER_MIN, sec % SEC_PER_MIN);
+	}
+	return 0;
+}
