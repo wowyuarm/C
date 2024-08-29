b4666408aa5bfce5b52e053e23b9383e6b8481c8
wowyuarm <wuyuang05@outlook.com>
2024-08-29 18:08:30 +0800

    C49

---
+++ testC49.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+void pound(int n);
+int main(void)
+{
+    int a = 5;
+    char ch = 'A';
+    float b = 6.0f;
+
+    pound(a);
+    pound(ch);
+    pound(b);
+
+    return 0;
+}
+
+void pound(int n)
+{
+    while (n-- > 0)
+        printf("#");
+    printf("\n");
+}
