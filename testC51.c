c45f90a6ca1a955d61f469e425dcb30a23e9d87d
wowyuarm <wuyuang05@outlook.com>
2024-08-31 18:47:27 +0800

    练习-C51

---
+++ testC51.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    const int daysperweek = 7;
+    int days, weeks, day_rem;
+
+    printf("Enter the number of days: ");
+    scanf("%d", &days);
+    while (days > 0)
+    {
+        weeks = days / daysperweek;
+        day_rem = days % daysperweek;
+        printf("%d days are %d weeks and %d days.\n",
+            days, weeks, day_rem);
+
+        printf("Enter the number of days (0 or less to end): ");
+        scanf("%d", &days);
+    }
+    printf("Done!\n");
+    return 0;
+}
