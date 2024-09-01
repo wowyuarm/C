751931470df38bab0625a8d13ed65302231a3a19
wowyuarm <wuyuang05@outlook.com>
2024-09-01 16:31:14 +0800

    练习-C52

---
+++ testC52.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main(void)
+{
+	int a, b;
+
+	printf("Enter a number:");
+	scanf("%d", &a);
+	b = a + 10;
+	while (a++ < b)
+		printf("%d\n", a);
+	return 0;
+}
