5dfce6b5e34d55446e8fe14d0f320693696b7f7a
wowyuarm <wuyuang05@outlook.com>
2024-09-04 18:26:42 +0800

    C56

---
+++ testC56.c
@@ -0,0 +1,10 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	for (printf("Enter a number which is smaller than 10:"); a != 3;)
+		scanf("%d", &a);
+	printf("Congratulation!");
+	return 0;
+}
