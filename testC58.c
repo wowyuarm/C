a8db56cd08127288d26982112b2949cc6b333bb5
wowyuarm <wuyuang05@outlook.com>
2024-09-05 18:20:04 +0800

    C58

---
+++ testC58.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#define CHAR 26
+int main()
+{
+	const int ROW = 26;
+	int chars, row;
+
+	for (row = 0; row < ROW; row++)
+	{
+		for (chars = ('Z' - row); chars > ('Z' - CHAR); chars--)
+			printf("%c", chars);
+		printf("\n");
+	}
+	return 0;
+}
