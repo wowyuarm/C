70bcb8f09cd221f966928272ebc1d893477d2053
wowyuarm <wuyuang05@outlook.com>
2024-09-07 17:29:44 +0800

    练习-C60

---
+++ testC60.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+double power(double n, int p);
+int main()
+{
+	double num, np;
+	int pow;
+	printf("计算幂 数字与指数:");
+	while (scanf("%lf %d", &num, &pow) == 2)
+	{
+		np = power(num, pow);
+		printf("结果为:%lf\n", np);
+		printf("继续/q放弃:");
+	}
+	return 0;
+}
+double power(double n, int p)
+{
+	double POW = 1;
+	int i;
+	for (i = 1; i <= p; i++)
+		POW *= n;
+	return POW;
+}
