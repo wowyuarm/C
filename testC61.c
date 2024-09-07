59a780511f378e0b51307388f17972857336659d
wowyuarm <wuyuang05@outlook.com>
2024-09-07 17:52:39 +0800

    练习-C61

---
+++ testC61.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int hang, note, HANG, NOTE;
+
+	printf("输入打印行数与字符数量:");
+	scanf("%d %d", &hang, &note);
+	for (HANG = 1; HANG <= hang; HANG++)
+	{
+		for (NOTE = 1; NOTE <= note; NOTE++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
