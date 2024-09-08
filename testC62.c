f38e82cda08de8041669c6208dc574d02839b120
wowyuarm <wuyuang05@outlook.com>
2024-09-08 19:52:17 +0800

    练习-C62

---
+++ testC62.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#define SIZE 8 
+int main(void)
+{
+    int twopows[SIZE];
+    int i;
+    int value = 1;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        twopows[i] = value;
+        value *= 2;
+    }
+
+    i = 0;
+    do
+    {
+        printf("%d ", twopows[i]);
+        i++;
+    } while (i < SIZE);
+    printf("\n");
+
+    return 0;
+}
