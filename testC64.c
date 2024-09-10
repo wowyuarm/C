30e098ff458a0745c5d7c09426cc691f9da3c7e0
wowyuarm <wuyuang05@outlook.com>
2024-09-10 18:30:59 +0800

    C64

---
+++ testC64.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#define SPACE " "
+int main(void)
+{
+	char ch;
+
+	while ((ch = getchar()) != "\n")
+	{
+		if (ch == SPACE)
+			putchar(ch);
+		else
+			putchar(ch + 1);
+	}
+
+	return 0;
+}
