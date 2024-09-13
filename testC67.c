fe9e46691714bef4b39e06fc2eacb811ee5cf0b0
wowyuarm <wuyuang05@outlook.com>
2024-09-13 20:03:29 +0800

    练习-C67

---
+++ testC67.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h> 
+int main(void)
+{
+    int n;
+    double sumeven = 0.0;
+    int ct_even = 0;
+    double sumodd = 0.0;
+    int ct_odd = 0;
+
+    while (scanf("%d", &n) == 1 && n != 0)
+    {
+        if (n % 2 == 0)
+        {
+            sumeven += n;
+            ++ct_even;
+        }
+        else
+        {
+            sumodd += n;
+            ++ct_odd;
+        }
+    }
